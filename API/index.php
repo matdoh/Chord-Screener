7a8fe5a540aa76a5ac5d79ec8415b9483f346379
mado958f <matthias_benno.dohlenburg@mailbox.tu-dresden.de>
2024-09-06 12:05:41 +0200

    initial commit

---
+++ API/index.php
@@ -0,0 +1,47 @@
+
+<?php
+$methode = $_SERVER['REQUEST_METHOD'];
+$req = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$wanted = NULL;
+if(!empty($req[0])) {
+    if(strtolower($req[0]) == 'sb') {
+        if(!empty($_GET['song'])) {$wanted = $_GET['song'];}
+        else {$wanted = "list";}
+    }
+}
+$data = json_decode(substr(file_get_contents('files/dataFile.txt'), 3));
+
+switch ($methode) {
+    case 'GET':
+        //echo (json_encode($req . " " . $_GET['cs'] . " " . $daten));
+        switch ($wanted) {
+            case NULL: echo '404: You are looking for something that isn\'t here.';
+            case "list": // Verzeichnis auflisten
+                $me = $data->songs;
+                $returnarray = array();
+
+                foreach($me as $m) {
+                    $returnarray[] = [$m->name, $m->author, $m->DeepSearch];
+                }
+
+                echo json_encode($returnarray);
+            default:
+                foreach ($data->songs as $song) {
+                    if($song->name == $wanted) {
+                        echo json_encode($song);
+                    }
+                }
+
+        } //else {echo (file_get_contents('files/'.$dateiname));}
+        break;
+    /*case 'POST':
+        file_put_contents('files/'.$dateiname, $data);
+        break;
+    case 'PUT':
+        file_put_contents('files/'.$dateiname, $data, FILE_APPEND);
+        break;
+    case 'DELETE':
+        unlink('files/'.$dateiname);
+        break;*/
+}
+?>
