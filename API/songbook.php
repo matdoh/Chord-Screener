345ec4689f68f6668dc26146f2a0fba5e7b01e03
LordPhoenix44 <matthias_benno.dohlenburg@mailbox.tu-dresden.de>
2024-09-09 23:08:19 +0200

    release 1.1.0: Prettified songbook.php

--- API/songbook.php
+++ API/songbook.php
@@ -10,7 +10,6 @@ $data = json_decode(substr(file_get_contents('files/dataFile.txt'), 3));
 
 switch ($methode) {
     case 'GET':
-        //echo (json_encode($req . " " . $_GET['cs'] . " " . $daten));
         switch ($wanted) {
             case NULL: echo '404: You are looking for something that isn\'t here.';
             case "list": // Verzeichnis auflisten
@@ -31,16 +30,7 @@ switch ($methode) {
                     }
                 }
 
-        } //else {echo (file_get_contents('files/'.$dateiname));}
+        }
         break;
-    /*case 'POST':
-        file_put_contents('files/'.$dateiname, $data);
-        break;
-    case 'PUT':
-        file_put_contents('files/'.$dateiname, $data, FILE_APPEND);
-        break;
-    case 'DELETE':
-        unlink('files/'.$dateiname);
-        break;*/
 }
 ?>
